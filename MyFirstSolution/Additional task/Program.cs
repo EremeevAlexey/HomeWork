30ef5a45c7cc1f21ab1e504ddaadecc24a02f0ea
EremeevAlexey <Eremeev.Alexey.Sergeevich@gmail.com>
2021-07-23 12:17:45 +0300

    NewSolucition

---
+++ MyFirstSolution/Additional task/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Additional_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = 5;
+            Console.WriteLine(test);
+
+            int test2 = 777;
+            object test3 = test2;
+            Console.WriteLine(test3);   
+        }
+    }
+}
---
+++ MyFirstSolution/MyFirstConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyFirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int variable = 10;
+            Console.WriteLine(variable);
+            Console.ReadKey();
+           
+        }
+    }
+}
