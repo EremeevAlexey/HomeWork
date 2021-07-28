16bf248993337157593203d3658d3f03723581f5
EremeevAlexey <87819997+EremeevAlexey@users.noreply.github.com>
2021-07-28 23:36:25 +0300

    Update Program.cs

--- Lesson 3/GreatestCommonDivisor/Program.cs
+++ Lesson 3/GreatestCommonDivisor/Program.cs
@@ -55,47 +55,6 @@ namespace GreatestCommonDivisor
                         a = d;
                     }
                 }
-                Console.Write("Поиск наибольшего общего делителя.\nВведите количество чисел для проверки:");
-
-
-                int elementsArray = Convert.ToInt32(Console.ReadLine());
-                int[] array = new int[elementsArray];
-
-                for (int i = 0; i < array.Length; i++)
-                {
-                    Console.Write($"Введите число {i + 1}: ");
-                    array[i] = int.Parse(Console.ReadLine());
-                }
-
-                Array.Sort(array);
-
-                int valueA = array[0];
-                int valueB = array[array.Length];
-
-                while (valueA != 0)
-                {
-                    int remains = valueB % valueA;
-                    if (remains == 0)
-                    {
-                        Console.WriteLine("Наибольший общий делитель " + valueA);
-
-                    }
-
-                    else if (remains > 0)
-                    {
-
-                        valueB = valueA;
-                        valueA = remains;
-
-
-                    }
-
-                    else if (args.Length > 1)
-                    {
-                        valueA = array[array.Length - 1];
-
-                    }
-                }
             }
         }
     }
