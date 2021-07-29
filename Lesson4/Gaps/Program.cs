4716c807912f4e54cab8e4c666f5738ef8b581ab
EremeevAlexey <Eremeev.Alexey.Sergeevich@gmail.com>
2021-07-29 14:13:32 +0300

    NewSolition

---
+++ Lesson4/Gaps/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Gaps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Введите число для проверки промежутка: ");
+                string input = Console.ReadLine();
+                int convertInput = Convert.ToInt32(input);
+
+                if (convertInput <= 30)
+                {
+                    Console.WriteLine($"Число {convertInput} попадает в первый промежуток");
+                }
+
+                else if (convertInput > 30 && convertInput <= 60)
+                {
+                    Console.WriteLine($"Число {convertInput} попадает в второй промежуток");
+                }
+
+                else if (convertInput > 60 && convertInput <= 100)
+                {
+                    Console.WriteLine($"Число {convertInput} попадает в третий промежуток");
+                }
+
+                else
+                    Console.WriteLine($"Я не знаю такого числа как {convertInput}");
+
+            }
+            
+        }
+    }
+}
---
+++ Lesson4/MultipleOf17/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MultipleOf17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var = 200;
+
+            while (var != 500)
+            {
+                if (var % 17 == 0)
+                {
+                    Console.Write(var + ", ");
+                    var++;
+
+                }
+
+                else
+                    var++;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
