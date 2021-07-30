f52091074729b7d09e837918542b0113ea6f225a
EremeevAlexey <Eremeev.Alexey.Sergeevich@gmail.com>
2021-07-30 18:08:36 +0300

    NewSolution

--- Lesson 3/GreatestCommonDivisor/Program.cs
+++ Lesson 3/GreatestCommonDivisor/Program.cs
@@ -1,61 +1,74 @@
 ﻿using System;
+using GreatestCommonDivisor;
 
 namespace GreatestCommonDivisor
 {
     class Program
     {
-        static void Main(string[] args)
+        //Создаем метод для расчета наибольшего общего делителя.
+        static int NewDevisor(int numberA, int numberB)
         {
             while (true)
             {
-                Console.WriteLine("Поиск наибольшего общего делителя.\nВведите число A");
-                string line1 = Console.ReadLine();
-                int a = Convert.ToInt32(line1);
-
-                Console.WriteLine("Введите число B");
-                string line2 = Console.ReadLine();
-                int b = Convert.ToInt32(line2);
-
-                if (a >= b)
+                int devisor = numberA % numberB;
+                if (devisor == 0)
                 {
-
-                    while (b != 0)
-                    {
-                        //находим наибольший общий делитель
-                        int c = a % b;
-                        int f = a / b;
-                        Console.WriteLine(a + " делится на " + b + " всего " + f + " раз с остатком от деления " + c);
-                        if (c == 0)
-                        {
-                            Console.WriteLine("Наибольший общий делитель " + b);
-                        }
-                        //переопределяем значение переменных для следующего цикла
-                        int d = c;
-                        a = b;
-                        b = d;
-
-                    }
+                    return numberB;
                 }
-
                 else
                 {
-                    while (a != 0)
-                    {
-                        //находим наибольший общий делитель
-                        int c = b % a;
-                        int f = b / a;
-                        Console.WriteLine(b + " делится на " + a + " всего " + f + " раз с остатком от деления " + c);
-                        if (c == 0)
-                        {
-                            Console.WriteLine("Наибольший общий делитель " + a);
-                        }
-                        //переопределяем значение переменных для следующего цикла
-                        int d = c;
-                        b = a;
-                        a = d;
-                    }
+                    int c = devisor;
+                    numberA = numberB;
+                    numberB = c;
+                    continue;
                 }
+
             }
+        }
+
+
+        static void Main(string[] array)
+        {
+            //создаем запрос в консоли на количество элементов массиве.
+            Console.Write("Поиск наибольшего общего делителя.\nВведите количество чисел для проверки:");
+
+            //ситываем строку и создаем массив.
+            int elementsArray = Convert.ToInt32(Console.ReadLine());
+            int[] myArray = new int[elementsArray];
+
+            //заполняем массив через консоль. 
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write($"Введите число {i + 1}: ");
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            //преобразуем массив что-бы числа шли по убыванию.
+            Array.Sort(myArray);
+            Array.Reverse(myArray);
+
+
+            //считаем наибольший общий делитель для первых двух чисел через созданный метод.
+            int devisor1 = NewDevisor(myArray[0], myArray[1]);
+
+            /*проверяем все элементы массива на наибольший общий делитель.
+            Если не подходит то предаем новые заначения для расчета*/
+            for (int j = 0; j < myArray.Length; j++)
+            {
+
+                int result = myArray[j] % devisor1;
+                if (result != 0)
+                {
+                    int devisor2 = NewDevisor(myArray[j], devisor1);
+                    devisor1 = devisor2;
+                }
+                else
+                    continue;
+            }
+
+            //Вывод наибольшего общего делителя.
+            Console.WriteLine($"Наибольший общий делитель для этих {elementsArray} чисел равна {devisor1}.");
+            Console.ReadKey();
         }
     }
 }
