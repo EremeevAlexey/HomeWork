cf316099191c6884854da3e8e4b5060129be643a
EremeevAlexey <Eremeev.Alexey.Sergeevich@gmail.com>
2021-09-01 11:51:26 +0200

    Lesson 10

---
+++ Lesson10/ClassLibrary/MyList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class MyList : IList<MyItem>
+    {
+        private List<MyItem> objectMyItem = new List<MyItem>();
+
+        public MyItem this[int index]
+        {
+            get { return objectMyItem[index]; }
+            set { objectMyItem[index] = value; }
+        }
+
+        public int Count
+        {
+            get { return objectMyItem.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public void Add(MyItem item)
+        {
+            objectMyItem.Add(item);
+        }
+
+        public void Clear()
+        {
+            objectMyItem.Clear();
+        }
+
+        public bool Contains(MyItem item)
+        {
+            return objectMyItem.Contains(item);
+        }
+
+        public void CopyTo(MyItem[] array, int arrayIndex)
+        {
+            objectMyItem.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<MyItem> GetEnumerator()
+        {
+            return objectMyItem.GetEnumerator();
+        }
+
+        public int IndexOf(MyItem item)
+        {
+            return objectMyItem.IndexOf(item);
+        }
+
+        public void Insert(int index, MyItem item)
+        {
+            objectMyItem.Insert(index, item);
+        }
+
+        public bool Remove(MyItem item)
+        {
+            return objectMyItem.Remove(item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            objectMyItem.RemoveAt(index);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return objectMyItem.GetEnumerator();
+        }
+    }
+}
---
+++ Lesson10/ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ClassLibrary;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyItem item = new MyItem();
+            item.newObject = "Что то";
+
+            MyList items = new MyList();
+            items.Add(item);
+
+            MyItem item2 = new MyItem();
+            item2.newObject = "Другое что то";
+            items.Add(item);
+
+            MyItem item3 = new MyItem();
+            item3.newObject = "Третье что то";
+            items.Insert(1, item);
+
+            items.RemoveAt(2);
+
+            Console.WriteLine(items.Count);
+
+            items.Clear();
+ 
+
+
+        }
+    }
+}
