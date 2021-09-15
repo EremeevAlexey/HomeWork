bbcba24a6fb5880a90034ab4adaea535da0b0f8b
EremeevAlexey <Eremeev.Alexey.Sergeevich@gmail.com>
2021-09-15 22:52:15 +0300

    Lesson 15

---
+++ Lesson15/ClassLibrary/SearchEngine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+
+namespace ClassLibrary
+{
+    public class SearchEngine
+    {
+        public FileInfo Search(DirectoryInfo directoryInf, string fileName)
+        {
+            foreach (var file in directoryInf.GetFiles())
+            {
+                if (fileName == file.Name)
+                {
+                    return file;
+                }
+
+            }
+            foreach (var directory in directoryInf.GetDirectories())
+            {
+                try
+                {
+                    return Search(directory, fileName);
+                }
+                catch (FileNotFoundException)
+                {
+
+                }
+            }
+            throw new FileNotFoundException($"Файл {fileName} не найден");
+
+        }
+    }
+}
---
+++ Lesson15/ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using ClassLibrary;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Где будем искать?");
+            string directory = Console.ReadLine();
+            DirectoryInfo directoryInfo = new DirectoryInfo(directory);
+
+            Console.WriteLine("Какой файл нужно найти?");
+            var file = Console.ReadLine();
+            SearchEngine searchFile = new SearchEngine();
+            var resultSearch = searchFile.Search(directoryInfo, file);
+
+            var packArhive = Path.Combine(resultSearch.Directory.FullName, Path.GetFileNameWithoutExtension(resultSearch.FullName) + ".zip");
+            using (var fileStream = new FileStream(packArhive, FileMode.Create))
+            using (ZipArchive zipArchive = new ZipArchive(fileStream, ZipArchiveMode.Create))
+            {
+                zipArchive.CreateEntryFromFile(resultSearch.FullName, resultSearch.Name);
+            }
+
+            Console.WriteLine($"Файл найден и успешно упакован. Путь к архиву: {packArhive}");
+        }
+    }
+}
